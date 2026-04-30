56eadb1b57c7811452e7008d20be9ee38cae5f03
Andreidy2207 <davidandreidy2207@gmail.com>
2026-04-30 02:19:07 -0600

    aplicacion usando vue js y bootstrap

---
+++ mi-app/src/views/ProductoView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mt-4">
+    <h3>Gestión de Productos</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { productos: [], nuevo: { nombre: '', precio: '' } }
+  },
+  // ... resto de la lógica ...
+}
+</script>
